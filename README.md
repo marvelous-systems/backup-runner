7e65404699c467ad91ee57eff8d3fd2ebc461719
Noah Hummel <n.p.hummel@gmail.com>
2019-02-06 15:26:54 +0100

    [FEATURE] run locally or inside cluster

--- README.md
+++ README.md
@@ -11,9 +11,16 @@ $ . .venv/bin/activate
 $ pip install -r requirements.txt
 ```
 
+## Running locally using kubeconfig
+
+```bash
+$ docker run -v ~/.kube/config:/kube/config backup-runner
+```
+
 # Building
 
 ```bash
+$ docker build -t backup-runner .
 ```
 
 # Usage
--- app/src/main.py
+++ app/src/main.py
@@ -1,4 +1,28 @@
+import sys
+
+import os
+
 __author__ = "Noah Hummel"
 
+import logging
+
+from kubernetes import client, config
+
+
 if __name__ == "__main__":
-    print("Hello, backup-runner!")
+    logger = logging.getLogger("backup-runner")
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    if os.environ.get("KUBERNETES_PORT"):  # running inside cluster
+        config.load_incluster_config()
+    else:
+        config.load_kube_config("/kube/config")
+
+    appsV1 = client.AppsV1Api()
+    logger.debug("Hello, Kubernetes!")
+    logger.debug(appsV1.list_deployment_for_all_namespaces())
