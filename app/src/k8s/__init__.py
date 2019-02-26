3d8b6c70ff804fbb9f0a2923da4809e04b573dd0
Noah Hummel <n.p.hummel@gmail.com>
2019-02-26 14:46:56 +0100

    [FEATURE] add k8s.wait_for_total_reconciliation

--- app/src/k8s/__init__.py
+++ app/src/k8s/__init__.py
@@ -2,13 +2,16 @@ import asyncio
 
 from datetime import timedelta
 
-from typing import Callable
+from typing import Callable, Tuple, List, Dict, Coroutine
 
 import logger
 from mutations.exceptions import ReconciliationError
 
 __author__ = "Noah Hummel"
 log = logger.get(__name__)
+
+
+Predicate = Callable
 
 
 def check_resource(predicate: Callable, fn: Callable, *args, **kwargs) -> bool:
@@ -27,9 +30,11 @@ def check_resource(predicate: Callable, fn: Callable, *args, **kwargs) -> bool:
     """
     result = fn(*args, **kwargs)
     retval = predicate(result)
-    log.debug(f"Checking {result.kind} "
-              f"{result.metadata.namespace}/{result.metadata.name}: "
-              f"{predicate.__name__}? {retval}")
+    try:  # debug print only works for k8s resources
+        log.debug(f"Checking {result.kind} "
+                  f"{result.metadata.namespace}/{result.metadata.name}: "
+                  f"{predicate.__name__}? {retval}")
+    except Exception: pass
     return retval
 
 
@@ -39,18 +44,46 @@ def wait_for_reconciliation(predicate: Callable, timeout: timedelta,
                                          **kwargs))
 
 
-async def _wait_for_reconciliation(predicate: Callable, timeout: timedelta,
-                                   fn: Callable, *args, **kwargs):
+def _reconciled_fn(predicate,fn, *args, **kwargs) -> Coroutine:
     async def _reconciled():
         done = False
         while not done:
             done = check_resource(predicate, fn, *args, **kwargs)
             await asyncio.sleep(3, 6)
+    return _reconciled()
 
+
+async def _wait_for_reconciliation(predicate: Callable, timeout: timedelta,
+                                   fn: Callable, *args, **kwargs):
     try:
         log.debug(f"Waiting for cluster state to reconcile "
                   f"({timeout.seconds}s)...")
-        await asyncio.wait_for(_reconciled(), timeout=timeout.seconds)
+        await asyncio.wait_for(_reconciled_fn(predicate, fn, *args, **kwargs),
+                               timeout=timeout.seconds)
     except asyncio.TimeoutError:
         log.debug(f"Cluster state not reconciled after {timeout.seconds}s.")
         raise ReconciliationError()
+
+
+def wait_for_total_reconciliation(timeout: timedelta,
+                                  *args: Tuple[Predicate, Callable, List, Dict]):
+    asyncio.run(_wait_for_total_reconciliation(timeout, *args))
+
+
+async def _wait_for_total_reconciliation(timeout: timedelta,
+                                  *args: Tuple[Predicate, Callable, List, Dict]):
+    tasks = []
+    for state in args:
+        state = list(state)  # tuples are immutable
+        if len(state) < 3:
+            state.append([])  # no *args
+        if len(state) < 4:
+            state.append(dict())  # no **kwargs
+
+        tasks.append(_reconciled_fn(state[0], state[1], *state[2], **state[3]))
+
+    awaitable = asyncio.gather(*tasks)
+    try:
+        await asyncio.wait_for(awaitable, timeout=timeout.seconds)
+    except TimeoutError:
+        raise ReconciliationError
