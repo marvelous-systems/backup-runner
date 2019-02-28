11531191ea4428327eb465c370dd2849d202a83e
Noah Hummel <n.p.hummel@gmail.com>
2019-02-28 12:39:59 +0100

    [TASK] refactor mutations

--- app/src/mutations/scale.py
+++ app/src/mutations/scale.py
@@ -1,5 +1,5 @@
-import random
 import asyncio
+
 from datetime import timedelta
 
 import sentry_sdk
@@ -7,7 +7,7 @@ from kubernetes import client
 from kubernetes.client.rest import ApiException
 
 import logger
-from k8s import check_resource, wait_for_reconciliation
+from k8s import wait_for_reconciliation
 from k8s.predicate import deployment_has_scale
 from mutations.exceptions import ReconciliationError
 from views.scale import get_scale_for_deployment
@@ -16,40 +16,73 @@ __author__ = "Noah Hummel"
 log = logger.get(__name__)
 
 
-def scale_deployment(name: str, namespace: str, replicas: int):
+def update_deployment_scale(name: str, namespace: str, replicas: int):
+    """Updates a Deployment to scale to a certain amount of replicas.
+
+    Does not wait for the cluster state to update to the desired state.
+
+    Args:
+        name: Name of the Deployment.
+        namespace: Namespace of the Deployment.
+        replicas: Desired number of replicas.
+    """
     try:
-        appsV1 = client.AppsV1Api()
-        current_scale = appsV1.read_namespaced_deployment_scale(name, namespace)
+        api = client.AppsV1Api()
+        current_scale = api.read_namespaced_deployment_scale(name, namespace)
         current_scale.spec.replicas = replicas
-        new_scale = appsV1.patch_namespaced_deployment_scale(name, namespace,
-                                                             current_scale)
+        new_scale = api.patch_namespaced_deployment_scale(name, namespace,
+                                                          current_scale)
         log.debug(f"Updated deployment {namespace}/{name} spec to "
                   f"{new_scale.spec.replicas} replicas")
     except ApiException as e:
         sentry_sdk.capture_exception(e)
 
 
-def scale_deployment_sync(name: str, namespace: str, replicas: int,
-                          timeout: timedelta):
-    """
-    Same as scale_deployment, but waits for deployment scale to reach target.
-    Returns and logs a warning after a certain timeout, if the deployment was
-    not scaled to the target replica count.
+async def scale_deployment(name: str, namespace: str, replicas: int,
+                           timeout: timedelta):
+    """Updates a Deployment's scale and waits for cluster state to reconcile.
 
-    :param name: Name of the deployment
-    :param namespace: Namespace containing Deployment
-    :param replicas: Number of desired replicas
-    :param timeout: Time after which to return without success
+    Args:
+        name: Name of the Deployment.
+        namespace: Namespace of the Deployment.
+        replicas: Desired number of replicas.
+        timeout: Time to wait for reconciliation until failing.
+
+    Raises:
+        ReconciliationError:
+            The cluster state did not reconcile within timeout.
     """
     previous_replicas = get_scale_for_deployment(name, namespace)
-    scale_deployment(name, namespace, replicas)
+    update_deployment_scale(name, namespace, replicas)
 
     try:
-        v1 = client.AppsV1Api()
+        api = client.AppsV1Api()
         predicate = deployment_has_scale(replicas)
-        wait_for_reconciliation(predicate, timeout,
-                                v1.read_namespaced_deployment, name, namespace)
+        await wait_for_reconciliation(
+            predicate,
+            timeout,
+            api.read_namespaced_deployment,
+            name,
+            namespace
+        )
     except ReconciliationError:
         log.warning(f"Deployment {namespace}/{name} could not be scaled to "
                     f"{replicas}, reverting to {previous_replicas}.")
-        scale_deployment(name, namespace, previous_replicas)
+        update_deployment_scale(name, namespace, previous_replicas)
+
+
+def scale_deployment_blocking(name: str, namespace: str, replicas: int,
+                              timeout: timedelta):
+    """Updates a Deployment's scale and waits for cluster state to reconcile.
+
+    Args:
+        name: Name of the Deployment.
+        namespace: Namespace of the Deployment.
+        replicas: Desired number of replicas.
+        timeout: Time to wait for reconciliation until failing.
+
+    Raises:
+        ReconciliationError:
+            The cluster state did not reconcile within timeout.
+    """
+    asyncio.run(scale_deployment(name, namespace, replicas, timeout))
